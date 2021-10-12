74a3757e3bf3c90b0a541029f390ac05306ea150
Cole Cochran <colecochran405@gmail.com>
2021-10-12 17:22:21 -0500

    creating consts, adding the questions to be generated

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-// TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown")
+console.log(generateMarkdown)
+//import generateMarkdown from './utils/generateMarkdown.js'
+//import { writeFile} from 'fs'
 // TODO: Create an array of questions for user input
-const questions = [];
-
+inquirer
+ .prompt ([
+    {
+        type:"input",
+        name:'title',
+        message:"Enter the README.md title."
+    },{
+        type:'input',
+        name:'description',
+        message:"Enter the Description of the repository."
+    },{
+        type:'input',
+        name:'install',
+        message:"Add directions on how to install your application."
+    },{
+        type:'input',
+        name:'contributers',
+        message:"List your contributers to the project."
+    },{
+        type:'input',
+        name:'usage',
+        message:"Explain the use case for your application, and functionality."
+    },{
+        type:'checkbox',
+        name:'license',
+        message:"Select the license used for this project.",
+        choices:['MIT','MPL','IPL','EPL']
+    },{
+        type:'input',
+        name:'tests',
+        message:"List the tests used for the application."
+    }
+])
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
+function writeToFile(fileName, data) {
+    fs.writeFile("README.md",'\n',(err)=>
+    err ? console.log(err) : console.log('gennyMD4u'))
+//     fs.writeFile("README.md",JSON.stringify(fileName, data,'\t'),(err)=>
+//   err ? console.log(err) : console.log('success!')
+//   )
+ }
+//  )
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    //writeToFile(fileName,data)
+}
 // Function call to initialize app
-init();
+init()
